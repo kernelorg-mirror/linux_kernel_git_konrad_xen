6a2532180591495576493621347c75ba843aced9
Konrad Rzeszutek Wilk <konrad.wilk@oracle.com>
2011-10-17 17:30:51 -0400

    ttm/radeon/nouveau: Check the DMA address from TTM against known value.
    .. instead of checking against the DMA_ERROR_CODE value which is
    per-platform specific. The zero value is a known invalid value
    that the TTM layer sets on the dma_address array if it is not
    used (ttm_tt_alloc_page_directory calls drm_calloc_large which
    creates a page with GFP_ZERO).
    
    We can't use pci_dma_mapping_error as that is IOMMU
    specific (some check for a specific physical address, some
    for ranges, some just do a check against zero).
    
    Also update the comments in the header about the true state
    of that parameter.
    
    Reviewed-by: Jerome Glisse <jglisse@redhat.com>
    Signed-off-by: Konrad Rzeszutek Wilk <konrad.wilk@oracle.com>

--- drivers/gpu/drm/radeon/radeon_gart.c
+++ drivers/gpu/drm/radeon/radeon_gart.c
@@ -181,9 +181,7 @@ int radeon_gart_bind(struct radeon_device *rdev, unsigned offset,
 	p = t / (PAGE_SIZE / RADEON_GPU_PAGE_SIZE);
 
 	for (i = 0; i < pages; i++, p++) {
-		/* we reverted the patch using dma_addr in TTM for now but this
-		 * code stops building on alpha so just comment it out for now */
-		if (0) { /*dma_addr[i] != DMA_ERROR_CODE) */
+		if (dma_addr[i] != 0) {
 			rdev->gart.ttm_alloced[p] = true;
 			rdev->gart.pages_addr[p] = dma_addr[i];
 		} else {
