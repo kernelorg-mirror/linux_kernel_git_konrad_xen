9840681ee78b3af99454a04302df561be9fe7888
Konrad Rzeszutek Wilk <konrad.wilk@oracle.com>
2012-01-04 15:51:26 -0500

    xen/v86d: Fix compiler error when compiling under PPC64.
    Which does not have _PAGE_IOMAP defined.
    
    Signed-off-by: Konrad Rzeszutek Wilk <konrad.wilk@oracle.com>

--- drivers/char/mem.c
+++ drivers/char/mem.c
@@ -316,7 +316,9 @@ static int mmap_mem(struct file *file, struct vm_area_struct *vma)
 	vma->vm_flags |= VM_RESERVED | VM_IO | VM_PFNMAP | VM_DONTEXPAND;
 	vma->vm_page_prot =  __pgprot(
 			pgprot_val(vm_get_page_prot(vma->vm_flags)) |
+#ifdef _PAGE_IOMAP
 			_PAGE_IOMAP |
+#endif
 			pgprot_val(phys_mem_access_prot(file,
 				vma->vm_pgoff,
 				size,
