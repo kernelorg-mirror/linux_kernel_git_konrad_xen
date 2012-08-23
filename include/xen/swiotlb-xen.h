d815253cbf26a5273e77f829cc414e0808500263
Konrad Rzeszutek Wilk <konrad.wilk@oracle.com>
2012-08-23 14:36:15 -0400

    xen/swiotlb: Use the swiotlb_late_init_with_tbl to init Xen-SWIOTLB late when PV PCI is used.
    With this patch we provide the functionality to initialize the
    Xen-SWIOTLB late in the bootup cycle - specifically for
    Xen PCI-frontend. We still will work if the user had
    supplied 'iommu=soft' on the Linux command line.
    
    CC: FUJITA Tomonori <fujita.tomonori@lab.ntt.co.jp>
    [v1: Fix smatch warnings]
    [v2: Added check for xen_swiotlb]
    [v3: Rebased with new xen-swiotlb changes]
    Signed-off-by: Konrad Rzeszutek Wilk <konrad.wilk@oracle.com>

--- include/xen/swiotlb-xen.h
+++ include/xen/swiotlb-xen.h
@@ -3,7 +3,7 @@
 
 #include <linux/swiotlb.h>
 
-extern void xen_swiotlb_init(int verbose);
+extern int xen_swiotlb_init(int verbose);
 
 extern void
 *xen_swiotlb_alloc_coherent(struct device *hwdev, size_t size,
