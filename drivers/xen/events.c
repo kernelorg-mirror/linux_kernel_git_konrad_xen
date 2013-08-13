c5d297e088a081aaba61dfeb8afda94f922963af
Konrad Rzeszutek Wilk <konrad.wilk@oracle.com>
2013-08-13 13:01:38 -0400

    xen/events: BUG_ON if the events are sent to an offline CPU.
    B/c when the CPU does go online it will reset its event channels.
    
    So whatever event we want to go there, should wait until the
    CPU is on.
    
    Signed-off-by: Konrad Rzeszutek Wilk <konrad.wilk@oracle.com>

--- drivers/xen/events.c
+++ drivers/xen/events.c
@@ -1214,6 +1214,7 @@ void xen_send_IPI_one(unsigned int cpu, enum ipi_vector vector)
 {
 	int irq = per_cpu(ipi_to_irq, cpu)[vector];
 	BUG_ON(irq < 0);
+	BUG_ON(!cpu_online(cpu));
 	notify_remote_via_irq(irq);
 }
 
