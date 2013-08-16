ee4c3a124c8c74a400647632f7b6f3deb369c35b
Konrad Rzeszutek Wilk <konrad.wilk@oracle.com>
2013-08-16 14:39:56 -0400

    xen/smp: Update pv_lock_ops before alternative code kicks in.
    Before this patch we would patch the pv_lock_ops sites and then
    later change the unlock_kick and lock_spinning to the Xen specific.
    
    That meant that for the core of the kernel we would be running
    with the baremetal version of unlock_kick and lock_spinning while
    for modules we would have the proper Xen specific slowpaths.
    
    As most of the module uses some API from the core kernel that ended
    up with slowpath lockers waiting forever to be kicked. And the
    kick never came b/c the unlock path that was taken was the
    baremetal one.
    
    On PV we do not have the problem as we initialize before the
    alternative code kicks in.
    
    Signed-off-by: Konrad Rzeszutek Wilk <konrad.wilk@oracle.com>

--- arch/x86/xen/smp.c
+++ arch/x86/xen/smp.c
@@ -737,4 +737,12 @@ void __init xen_hvm_smp_init(void)
 	smp_ops.cpu_die = xen_hvm_cpu_die;
 	smp_ops.send_call_func_ipi = xen_smp_send_call_function_ipi;
 	smp_ops.send_call_func_single_ipi = xen_smp_send_call_function_single_ipi;
+
+	/*
+ 	 * The alternative logic (which patches the unlock/lock) runs before
+ 	 * the smp bootup up code is activated. That meant we would never patch
+ 	 * the core of the kernel with proper paravirt interfaces but would patch
+ 	 * modules.
+ 	 */
+	xen_init_spinlocks();
 }
