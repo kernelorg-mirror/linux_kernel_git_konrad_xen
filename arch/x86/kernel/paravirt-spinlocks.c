4399f30536dbf396318fb812320330c8b6a5820e
Konrad Rzeszutek Wilk <konrad.wilk@oracle.com>
2013-08-26 13:47:48 -0400

    DEBUG: paravirt/spinlock/xen: Add a counter for baremetal slowpath.
    To help in figuring out the slowpath logic is working
    right.
    
    Signed-off-by: Konrad Rzeszutek Wilk <konrad.wilk@oracle.com>
    
    Conflicts:
    	arch/x86/xen/spinlock.c

--- arch/x86/kernel/paravirt-spinlocks.c
+++ arch/x86/kernel/paravirt-spinlocks.c
@@ -8,10 +8,27 @@
 
 #include <asm/paravirt.h>
 
+u64 taken_slow;
+u64 released_slow;
+
+EXPORT_SYMBOL(taken_slow);
+EXPORT_SYMBOL(released_slow);
+
+static void test_lock_spinning(struct arch_spinlock *lock, __ticket_t want)
+{
+       taken_slow++;
+}
+PV_CALLEE_SAVE_REGS_THUNK(test_lock_spinning);
+
+static void test_unlock_kick(struct arch_spinlock *lock, __ticket_t ticket)
+{
+       released_slow++;
+}
+
 struct pv_lock_ops pv_lock_ops = {
 #ifdef CONFIG_SMP
-	.lock_spinning = __PV_IS_CALLEE_SAVE(paravirt_nop),
-	.unlock_kick = paravirt_nop,
+	.lock_spinning = __PV_IS_CALLEE_SAVE(test_lock_spinning),
+	.unlock_kick = test_unlock_kick,
 #endif
 };
 EXPORT_SYMBOL(pv_lock_ops);
